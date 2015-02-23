462f28c651ca9c032a7e86ae5ab9c01f1d4be471
jsshin <jsshin@shineware.co.kr>
2015-02-24 01:28:56 +0900

    add chi-square feature selection example

--- src/kr/co/shineware/feature/selection/FeatureSelector.java
+++ src/kr/co/shineware/feature/selection/FeatureSelector.java
@@ -1,12 +1,16 @@
 package kr.co.shineware.feature.selection;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import kr.co.shineware.feature.model.CountMap;
 import kr.co.shineware.feature.model.CountedFeature;
 import kr.co.shineware.feature.model.LabeledFeature;
+import kr.co.shineware.util.common.collection.MapUtil;
+import kr.co.shineware.util.common.file.FileUtil;
 
 public class FeatureSelector {
 	private CountMap tfMap; //term freq.
@@ -38,13 +42,16 @@ public class FeatureSelector {
 	}
 
 	public void chisquare(){
+
 		Set<String> labels = cfMap.getKeys();
 		Set<String> terms = dfMap.getKeys();
 		int n = this.getTotalDocument(); //N
 		int count = 0;
 		for (String label : labels) {
-			Map<String,Double> scoreMap = new HashMap<String, Double>();
+			
 			int categoryFreq = this.getCategoryFreq(label); //A+C
+			Map<String,Double> selectedTermScoreMap = new HashMap<>();
+
 			for (String term : terms) {
 				int df = this.getDf(term); //A+B
 				int cooccurFreq = this.getCooccurrenceFreq(this.convertCooccurrenceKey(term, label)); //A
@@ -52,20 +59,57 @@ public class FeatureSelector {
 					continue;
 				}
 				double score = this.getChisqrScore(n,df,categoryFreq,cooccurFreq);
+				selectedTermScoreMap.put(term, score);
 			}
+			selectedTermScoreMap = MapUtil.sortByValue(selectedTermScoreMap, MapUtil.DESCENDING_ORDER);
+			FileUtil.writeMap(selectedTermScoreMap, "features"+File.separator+label.replace("/", "_")+".txt");
 			count++;
 			System.out.println("Label Done "+count+"/"+labels.size()+" : "+label);
 		}
 	}
 
+	public String getLowestTerm(
+			Map<String, Double> selectedTermScoreMap) {
+		Set<Entry<String,Double>> entrySet = selectedTermScoreMap.entrySet();
+		String lowestTerm = null;
+		double lowestScore = Double.MAX_VALUE;
+		for (Entry<String, Double> entry : entrySet) {
+			if(entry.getValue() < lowestScore){
+				lowestTerm = entry.getKey();
+				lowestScore = entry.getValue();
+			}
+		}
+		return lowestTerm;
+	}
+
 	private double getChisqrScore(int n, int df, int categoryFreq, int cooccurFreq) {
-		int normalizer = categoryFreq*(n-categoryFreq)*df*(n-df);
+		double normalizer = (double)categoryFreq*(n-categoryFreq)*df*(n-df);
 		int a = cooccurFreq;
 		int b = df-a;
 		int c = categoryFreq-a;
 		int d = n-(a+b+c);
+		if(a < 0){
+			System.out.println("a : "+a);
+		}
+		if(b < 0){
+			System.out.println("b : "+b);
+		}
+		if(c < 0){
+			System.out.println("c : "+c);
+		}
+		if(d < 0){
+			System.out.println("d : "+d);
+		}
+		if(normalizer < 0){
+			System.out.println("nor : "+normalizer);
+			System.out.println(categoryFreq);
+			System.out.println(n-categoryFreq);
+			System.out.println(df);
+			System.out.println(n-df);
+		}
 		double chi = n*Math.pow((double)a*d-(double)c*b, 2);
-		return normalizer/chi;
+		
+		return chi/normalizer;
 	}
 
 	private int getCooccurrenceFreq(String key) {
--- src/kr/co/shineware/feature/test/FeatureSelectorTest.java
+++ src/kr/co/shineware/feature/test/FeatureSelectorTest.java
@@ -1,16 +1,19 @@
 package kr.co.shineware.feature.test;
 
 import java.io.File;
+import java.util.List;
 
 import kr.co.shineware.feature.model.CountedFeature;
 import kr.co.shineware.feature.model.LabeledFeature;
 import kr.co.shineware.feature.selection.FeatureSelector;
+import kr.co.shineware.nlp.komoran.core.analyzer.Komoran;
+import kr.co.shineware.util.common.model.Pair;
 
 public class FeatureSelectorTest {
 
 	public static void main(String[] args) throws Exception {
+		Komoran komoran = new Komoran("models-light");
 		FeatureSelector selector = new FeatureSelector();
-//		LabeledFeature feature = convert();
 		HKIBParser parser = new HKIBParser();
 		parser.setDoc(new File("HKIB20000/HKIB-20000_005.txt"));
 		HKIBDoc doc = null;
@@ -18,11 +21,18 @@ public class FeatureSelectorTest {
 			LabeledFeature lf = new LabeledFeature();
 			lf.setLabel(doc.getCategory());
 			CountedFeature cf = new CountedFeature();
-			String text = doc.getText();
-			String[] tokens = text.split("\\s");
-			for (String token : tokens) {
-				cf.incFeature(token, 1);
+			String text = doc.getText().replaceAll("\n", " ");
+			
+			@SuppressWarnings("unchecked")
+			List<List<Pair<String,String>>> results = komoran.analyze(text);
+			for (List<Pair<String, String>> result : results) {
+				for (Pair<String, String> pair : result) {
+					if(pair.getSecond().startsWith("N")){
+						cf.incFeature(pair.getFirst()+"/"+pair.getSecond(), 1);
+					}
+				}
 			}
+
 			lf.setFeature(cf);
 			selector.addFeature(lf);
 		}
